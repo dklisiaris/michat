7cbf5c44f4f4660fc914cc7ef92b14d4a1441bc7
Dimitris Klisiaris <dklisiaris@gmail.com>
2015-08-09 01:58:02 +0300

    Change landing page and add login with github

---
+++ server/configuration/accounts.js
@@ -0,0 +1,10 @@
+ServiceConfiguration.configurations.upsert(
+  { service: "github" },
+  {
+    $set: {
+      clientId: "22c71f7b6614f0b741bc",
+      loginStyle: "popup",
+      secret: "e3d8d894926246e7ac2f72c7e1d2eb7cd14773df"
+    }
+  }
+);
